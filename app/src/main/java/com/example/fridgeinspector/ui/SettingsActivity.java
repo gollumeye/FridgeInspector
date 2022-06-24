cf6b8d02865f51fb49033b7da3b67d0ace466c77
Laura Randl <gollumeye@outlook.de>
2022-06-24 11:14:12 +0200

    added return Button in SettingsActivity

--- app/src/main/java/com/example/fridgeinspector/ui/SettingsActivity.java
+++ app/src/main/java/com/example/fridgeinspector/ui/SettingsActivity.java
@@ -2,9 +2,12 @@ package com.example.fridgeinspector.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.fridgeinspector.MainActivity;
 import com.example.fridgeinspector.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        FloatingActionButton returnFab = findViewById(R.id.returnFAB);
+        returnFab.setOnClickListener(e->{
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
