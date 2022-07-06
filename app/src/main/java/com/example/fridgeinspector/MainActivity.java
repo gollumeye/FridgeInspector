4834085c40dae77699dc418ba5bd5f18d2766e76
Laura Randl <gollumeye@outlook.de>
2022-07-06 21:59:38 +0200

    added new Dialog for adding Recipes

--- app/src/main/java/com/example/fridgeinspector/MainActivity.java
+++ app/src/main/java/com/example/fridgeinspector/MainActivity.java
@@ -16,9 +16,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -35,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private AlertDialog dialog;
+    private AlertDialog addRecipeDialog;
     private static int themeColor = 0;
 
     @Override
@@ -131,6 +134,53 @@ public class MainActivity extends AppCompatActivity {
         builder.setView(viewAddDialog);
         dialog = builder.create();
 
+
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+        builder2.setTitle("Enter Recipe Data:");
+        View viewAddDialog2 = getLayoutInflater().inflate(R.layout.add_recipe_dialog, null);
+        EditText name = viewAddDialog.findViewById(R.id.addRecipeTitle);
+        Button addRecipe, addIngridient, cancelRecipeButton;
+        addRecipe = viewAddDialog2.findViewById(R.id.addRecipeButton);
+        addIngridient = viewAddDialog2.findViewById(R.id.addIngridientButton);
+        cancelRecipeButton = viewAddDialog2.findViewById(R.id.button3);
+
+
+        addRecipe.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String name = eName.getText().toString();
+                String category = sCategory.getSelectedItem().toString();
+                String quantity = sQuantity.getSelectedItem().toString();
+                Calendar calendar = new GregorianCalendar(dExpirationDate.getYear(),
+                        dExpirationDate.getMonth(),
+                        dExpirationDate.getDayOfMonth());
+                Date expirationDate = calendar.getTime();
+
+                /*CategoryListFragment catFr = new CategoryListFragment();
+                String [] array = {name, category,expirationDate.toString(), quantity };
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                Bundle bundle = new Bundle();
+                bundle.putStringArray("key", array);
+
+                CategoryListFragment categoryListFragment = new CategoryListFragment();
+                categoryListFragment.setArguments(bundle);
+                fragmentTransaction.commit();*/
+
+                addRecipeDialog.dismiss();
+
+                Snackbar.make(view, "Adding Item", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        cancelRecipeButton.setOnClickListener(view2 -> addRecipeDialog.dismiss());
+        builder2.setView(viewAddDialog2);
+        addRecipeDialog = builder2.create();
+
         FloatingActionButton fab = findViewById(R.id.add_fab);
         switch(getThemeColor()){
             case 0:
@@ -162,7 +212,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                dialog.show();
+                int id = navController.getCurrentDestination().getId();
+                if(id==R.id.navigation_home){
+                    dialog.show();
+                }
+                else if (id==R.id.navigation_dashboard){
+                    addRecipeDialog.show();
+                }
 
             }
         });
